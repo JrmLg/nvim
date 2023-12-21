b8a188cab87d96fc49d762eb3c3e544145430fcc
JrmLg <63566435+JrmLg@users.noreply.github.com>
2023-12-21 11:06:10 +0100

    Updated translation shortcuts.

--- lua/plugins/translate-conf.lua
+++ lua/plugins/translate-conf.lua
@@ -3,30 +3,30 @@ return {
 
 	config = function()
 		vim.keymap.set("v", "<Leader>t", ":Translate en<CR>", { noremap = true, desc = "[T]ranslate in english." })
-		vim.keymap.set("v", "<Leader>tf", ":Translate fr<CR>", { noremap = true, desc = "[T]ranslate in english." })
+		vim.keymap.set("v", "<Leader>tf", ":Translate fr<CR>", { noremap = true, desc = "[T]ranslate in french." })
 		vim.keymap.set(
 			"v",
 			"<Leader>tr",
 			":Translate en -output=replace<CR>",
-			{ noremap = true, desc = "[T]ranslate in english." }
+			{ noremap = true, desc = "[T]ranslate and [R]eplace in english." }
 		)
 		vim.keymap.set(
 			"v",
 			"<Leader>trf",
 			":Translate fr -output=replace<CR>",
-			{ noremap = true, desc = "[T]ranslate in english." }
+			{ noremap = true, desc = "[T]ranslate and [R]eplace in french." }
 		)
 		vim.keymap.set(
 			"v",
 			"<Leader>ti",
 			":Translate en -output=insert<CR>",
-			{ noremap = true, desc = "[T]ranslate in english." }
+			{ noremap = true, desc = "[T]ranslate and [I]nsert in english." }
 		)
 		vim.keymap.set(
 			"v",
 			"<Leader>tif",
-			":Translate en -output=insert<CR>",
-			{ noremap = true, desc = "[T]ranslate in english." }
+			":Translate fr -output=insert<CR>",
+			{ noremap = true, desc = "[T]ranslate and [I]nsert in french." }
 		)
 	end,
 }
